177da7930628ccf4d3eefe674783794f8c7efe26
libingkui <13938562465@163.com>
2020-02-20 12:55:38 +0800

    create

---
+++ src/main/java/cn/org/bnc/opsManager/OpsManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.org.bnc.opsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/org/bnc/opsManager/OpsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.bnc.opsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
